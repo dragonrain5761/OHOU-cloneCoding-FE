b5d7fd22583541e14c09245d9ea1f1663704a6e7
nayoung3669 <skdud3669@gmail.com>
2023-07-21 16:10:21 +0900

    feat: set up Router

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,5 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "../pages/pages/MainPage";
+import SearchPage from "../pages/pages/SearchPage";
+import ArticlePage from "../pages/pages/ArticlePage";
+import HelpPage from "../pages/pages/HelpPage";
+import WritePage from "../pages/pages/WritePage";
+import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
+
 const Router = () => {
-  return <div></div>;
+  //api명세서 따라서 path 수정해주세요
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/article" element={<ArticlePage />} />
+        <Route path="/help" element={<HelpPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
