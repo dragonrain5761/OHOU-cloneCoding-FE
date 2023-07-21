5cce2d8c4716a01c0cab05f0a8620be3ac9ce470
leolego03 <leolego03@gmail.com>
2023-07-21 23:40:20 +0900

    feat: Add product information component

--- src/containers/product/ProductContainer.js
+++ src/containers/product/ProductContainer.js
@@ -1,8 +1,13 @@
-import Product from "../../components/product/Product"
+import Product from "../../components/product/Product";
+import ProductInfo from "../../components/product/ProductInfo";
+import styles from "./ProductContainer.module.css";
 
 const ProductContainer = () => {
   return (
-    <Product />
+    <div className={styles.ProductContainer}>
+      <Product />
+      <ProductInfo />
+    </div>
   )
 }
 
