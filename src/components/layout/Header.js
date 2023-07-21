34c4eef495294fd33758f3dc390d46676b2488f5
nayoung3669 <skdud3669@gmail.com>
2023-07-22 01:23:34 +0900

    feat: Header, main page layout

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,27 @@
-import styled from "styled-components";
-import theme from "../../lib/styles/theme";
+import { Button, Input } from "../common";
+import { CiSearch } from "react-icons/ci";
+import HeaderBlock from "./Header.style";
+import src from "../../assets/logo.jpg";
 
 const Header = () => {
-  return <HeaderBlock>Header</HeaderBlock>;
+  return (
+    <HeaderBlock>
+      <p className="logo">
+        <img src={src} alt="logo" />
+      </p>
+      <div className="nav">
+        <Input w={"250px"} h={"40px"} placeholder={"통합검색"} />
+        <Button size={"small"}>로그인</Button>
+        <Button size={"small"}>회원가입</Button>
+        <p className="searchIcon">
+          <CiSearch />
+        </p>
+        <Button size={"small"} color={"primary"}>
+          글쓰기
+        </Button>
+      </div>
+    </HeaderBlock>
+  );
 };
 
 export default Header;
-
-const HeaderBlock = styled.div`
-  display: ${theme.flexCenter};
-`;
