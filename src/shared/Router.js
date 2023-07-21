7e2c799fe50856064230cb0b33f0ced26ddd00c9
leolego03 <leolego03@gmail.com>
2023-07-21 19:19:03 +0900

    feat: Add product page

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../pages/pages/MainPage";
 import SearchPage from "../pages/pages/SearchPage";
 import ArticlePage from "../pages/pages/ArticlePage";
+import ProductPage from "../pages/pages/ProductPage";
 import HelpPage from "../pages/pages/HelpPage";
 import WritePage from "../pages/pages/WritePage";
 import LoginPage from "../pages/auth/LoginPage";
@@ -15,6 +16,7 @@ const Router = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/article" element={<ArticlePage />} />
+        <Route path="/product" element={<ProductPage />} />
         <Route path="/help" element={<HelpPage />} />
         <Route path="/write" element={<WritePage />} />
         <Route path="/login" element={<LoginPage />} />
