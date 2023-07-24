0a25db498ce42ae550498d58da5f195bc8d88aa6
leolego03 <leolego03@gmail.com>
2023-07-24 20:10:12 +0900

    feat: add write component

--- src/components/write/Write.js
+++ src/components/write/Write.js
@@ -1,5 +1,63 @@
-const Write = () => {
-  return <div>Write</div>;
+import styles from "./Write.module.css";
+
+const Write = ({
+  title,
+  content,
+  navigate,
+  inputRef,
+  onChangeTitle,
+  onChangeContent,
+  onChangeImage,
+  onClickSubmitButton
+}) => {
+  return (
+    <>
+      <div className={styles.ItemAdd}>
+        {/* Form area */}
+        <h3>Write...!</h3>
+
+        <form onSubmit={onClickSubmitButton}>
+          <div className={styles.ItemAdd_input_container}>
+            <label>Title</label>
+            <input
+              type='text'
+              value={title}
+              onChange={onChangeTitle}
+            />
+          </div>
+
+          <div className={styles.ItemAdd_textarea_container}>
+            <label>Content</label>
+            <textarea
+              value={content}
+              onChange={onChangeContent}
+            >
+            </textarea>
+          </div>
+
+          <div className={styles.ItemAdd_input_container}>
+            <label>Image</label>
+            <input
+              ref={inputRef}
+              type='file'
+              accept='image/*'
+              onChange={onChangeImage}
+            />
+          </div>
+
+          <div className={styles.ItemAdd_button_container}>
+            <button type="submit">Add</button>
+            <button onClick={() => {
+              navigate('/')
+            }}
+            >
+              Back
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
 };
 
 export default Write;
