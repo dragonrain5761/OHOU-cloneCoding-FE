091fc21741757f0900da6937bbe4d39357283088
leolego03 <leolego03@gmail.com>
2023-07-24 17:04:33 +0900

    feat: add useQuery to product component

--- src/api/item.js
+++ src/api/item.js
@@ -7,6 +7,13 @@ export const getItems = async (size = 8, page) => {
   return res;
 };
 
+//상품 조회
+export const getItem = async (itemId) => {
+  const api = defaultInstance();
+  const res = await api.get(`items/${itemId}`);
+  return res;
+}
+
 //상품 검색
 export const getSearchItems = async (keyword, size = 12, page) => {
   const api = defaultInstance();
