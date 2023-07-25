18412ef7bd77ac7c833879c9d3fdeac6ee145c77
nayoung3669 <skdud3669@gmail.com>
2023-07-25 11:47:43 +0900

    fix: template page 변경 및 router에 추가

--- src/containers/layout/Template.js
+++ src/containers/layout/Template.js
@@ -1,9 +1,11 @@
 import Footer from "../../components/layout/Footer";
+import Header from "../../components/layout/Header";
 import TemplateBlock from "./Template.style";
 
-const Template = ({ children, footer }) => {
+const Template = ({ children, header, footer }) => {
   return (
     <>
+      {header && <Header />}
       <TemplateBlock>{children}</TemplateBlock>
       {footer && <Footer />}
     </>
