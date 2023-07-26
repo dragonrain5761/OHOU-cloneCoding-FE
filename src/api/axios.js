0c0afacc6a57d346f4be0dcd403d4ae664067010
nayoung3669 <skdud3669@gmail.com>
2023-07-26 10:42:48 +0900

    feat: Access, Refresh Token interceptors

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const defaultInstance = () => {
   const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true,
+    // withCredentials: true,
   });
   return instance;
 };
@@ -11,7 +11,6 @@ const defaultInstance = () => {
 const authInstance = () => {
   const Access = localStorage.getItem("Access");
   const Refresh = localStorage.getItem("Refresh");
-  // console.log("token", token);
   const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
@@ -19,19 +18,50 @@ const authInstance = () => {
       Refresh: `Bearer ${Refresh}`,
     },
   });
+  instance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+      const accessTokenError = error.response.headers["TokenError"];
+      if (
+        accessTokenError &&
+        accessTokenError.includes("expired Access Token") &&
+        !originalRequest._retry
+      ) {
+        originalRequest._retry = true;
+        const newAccessToken = error.response.headers["Access"]; //변수명 확인 필요
+        localStorage.setItem("Access", newAccessToken);
+        originalRequest.headers["Access"] = `Bearer ${newAccessToken}`;
+        //이전 요청 재시도
+        console.log("새 access token 받음");
+        return instance(originalRequest);
+      }
+
+      if (
+        accessTokenError &&
+        accessTokenError.includes("expired Refresh Token") &&
+        !originalRequest._retry
+      ) {
+        localStorage.removeItem("Access");
+        localStorage.removeItem("Refresh");
+        console.log("refresh token 만료");
+        window.location.href = "/login";
+      }
+      return Promise.reject(error); // 그 외 에러 반환
+    },
+  );
   return instance;
 };
 
+//토큰 확인 필요
 const addInstance = () => {
   const Access = localStorage.getItem("Access");
-  const Refresh = localStorage.getItem("Refresh");
-  // const token = localStorage.getItem("token");
-  // console.log("token", token);
   const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
-      Authorization: `Bearer ${Access}`,
-      Refresh: `Bearer ${Refresh}`,
+      Access: `Bearer ${Access}`,
       "Content-Type": "multipart/form-data",
       //Refresh: refresh
     },
--- src/hooks/apis/useCommentQuery.js
+++ src/hooks/apis/useCommentQuery.js
@@ -1,9 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteComment, likeComment } from "../../api/comment";
+import { deleteComment, likeComment, postComment } from "../../api/comment";
 
 export const CommentQueryKey = "comment";
-
-//댓글 작성 추가
+export const usePostCommentMutation = () => {
+  const { mutate } = useMutation(postComment);
+  return mutate;
+};
 
 export const useDeleteCommenttMutation = () => {
   const { mutate } = useMutation(deleteComment);
