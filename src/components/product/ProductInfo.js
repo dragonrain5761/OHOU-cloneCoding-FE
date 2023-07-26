fa9ebb0944dadb705e54175852e751f2c5e5411d
leolego03 <leolego03@gmail.com>
2023-07-27 00:47:52 +0900

    design: apply responsive style to product detail page

--- src/components/product/ProductInfo.js
+++ src/components/product/ProductInfo.js
@@ -1,6 +1,3 @@
-import imgA from "../../assets/product/1667.webp";
-import imgB from "../../assets/product/1668.webp";
-import imgC from "../../assets/product/1669.avif";
 import {
   StyledProductInfo,
   ProductInfoInner,
@@ -8,30 +5,17 @@ import {
   OrderContainer
 } from "./ProductInfo.style";
 
-const ProductInfo = () => {
-  const options = [
-    "옵션 선택",
-    "01) 옵션 A",
-    "02) 옵션 B",
-    "03) 옵션 C"
-  ];
-  const moreItems = [
-    "추가상품 (선택)",
-    "01) 상품 A (3,000원)",
-    "02) 상품 B (3,000원)",
-    "03) 상품 C (3,000원)"
-  ];
-  const imgArray = [
-    imgA,
-    imgB,
-    imgC
-  ];
+const ProductInfo = ({
+  options,
+  moreItems,
+  imgArray
+}) => {
 
   return (
     <StyledProductInfo>
       <ProductInfoInner>
         <p>상품정보</p>
-        {imgArray.map((imgSrc, index) => (
+        {imgArray?.map((imgSrc, index) => (
           <ImageWrapper key={index}>
             <img src={imgSrc} alt="" />
           </ImageWrapper>
@@ -41,13 +25,13 @@ const ProductInfo = () => {
       <OrderContainer>
         <div>
           <select>
-            {options.map((option, index) => (
+            {options?.map((option, index) => (
               <option value={index} key={index}>{option}</option>
             ))}
           </select>
 
           <select>
-            {moreItems.map((item, index) => (
+            {moreItems?.map((item, index) => (
               <option value={index} key={index}>{item}</option>
             ))}
           </select>
--- src/containers/product/ProductContainer.style.js
+++ src/containers/product/ProductContainer.style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledProductContainer = styled.div `
   max-width: 1280px;
+  width: 100%;
   margin: 75px auto;
   padding: 20px;
 `;
