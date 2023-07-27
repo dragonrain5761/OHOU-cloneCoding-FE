9712fae1dec63334753606305f846e8440cbe6f4
leolego03 <leolego03@gmail.com>
2023-07-27 16:28:27 +0900

    fix: resolve upload, edit post error

--- src/api/axios.js
+++ src/api/axios.js
@@ -77,4 +77,38 @@ const addInstance = () => {
   return instance;
 };
 
-export { defaultInstance, authInstance, addInstance };
+const editInstance = () => {
+  const Access = localStorage.getItem("Access");
+  const instance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+      Access: `Bearer ${Access}`,
+    },
+  });
+
+  instance.interceptors.response.use(
+    (res) => {
+      if (res.headers.accesstokenerror) {
+        localStorage.setItem(
+          "Access",
+          res.headers.access.replace("Bearer ", ""),
+        );
+      }
+      return res;
+    },
+    async (error) => {
+      //refresh token만료
+      console.log(error);
+      localStorage.removeItem("Access");
+      localStorage.removeItem("Refresh");
+      console.log("refresh token 만료");
+      window.location.href = "/login";
+
+      return Promise.reject(error);
+    },
+  );
+
+  return instance;
+};
+
+export { defaultInstance, authInstance, addInstance, editInstance };
